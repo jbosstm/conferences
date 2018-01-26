0b1fa561ad8f76c1562f0d6b6c9e1775627d4057
Tom Jenkinson <tom.jenkinson@redhat.com>
2018-01-26 15:49:48 +0100

    DevConf 2018: Compensating Transactions for the Cloud

---
+++ devconf2018-cloud-transactions/3.sh
@@ -0,0 +1,1 @@
+java -jar hotel-service/target/lra-test-swarm.jar -Dswarm.http.port=8082
---
+++ devconf2018-cloud-transactions/hotel-service/src/main/java/io/narayana/rts/lra/demo/hotel/HotelMicroservice.java
@@ -0,0 +1,82 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2017, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package io.narayana.rts.lra.demo.hotel;
+
+import io.narayana.lra.annotation.Compensate;
+import io.narayana.lra.annotation.Complete;
+import io.narayana.lra.annotation.LRA;
+import io.narayana.lra.client.NarayanaLRAClient;
+import io.narayana.rts.lra.demo.model.Booking;
+import io.narayana.rts.lra.demo.model.BookingStore;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@RequestScoped
+@Path("/")
+public class HotelMicroservice {
+
+    @Inject
+    private BookingStore bookingStore;
+
+    // Business logic
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Booking reserve(@QueryParam("name") String name) {
+        Booking booking = new Booking(bookingId, name);
+        bookingStore.add(booking);
+        return booking;
+    }
+
+    @GET
+    @Path("/{bookingId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Booking get(@PathParam("bookingId") String bookingId) {
+        return bookingStore.get(bookingId);
+    }
+
+    // Participant
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    public Booking complete(String bookingId) {
+        return bookingStore.update(bookingId, Booking.BookingStatus.CONFIRMED);
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/compensate")
+    @Compensate
+    public Booking compensate(@HeaderParam(NarayanaLRAClient.LRA_HTTP_HEADER) String bookingId) {
+        return bookingStore.update(bookingId, Booking.BookingStatus.CANCELLED);
+    }
+}
