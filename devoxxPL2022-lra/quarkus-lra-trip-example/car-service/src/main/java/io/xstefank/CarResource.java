813921321e6745ec9477af9d2d179eb5384d53d9
xstefank <xstefank122@gmail.com>
2022-07-28 16:43:41 +0200

    Add devoxxPL2022

---
+++ devoxxPL2022-lra/quarkus-lra-trip-example/car-service/src/main/java/io/xstefank/CarResource.java
@@ -0,0 +1,52 @@
+package io.xstefank;
+
+import org.eclipse.microprofile.lra.annotation.Compensate;
+import org.eclipse.microprofile.lra.annotation.Complete;
+import org.eclipse.microprofile.lra.annotation.ws.rs.LRA;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/car")
+@ApplicationScoped
+public class CarResource {
+
+    @LRA(value = LRA.Type.MANDATORY, end = false)
+    @POST
+    @Path("/book")
+    public Response bookCarRental(@HeaderParam(LRA.LRA_HTTP_CONTEXT_HEADER) String lraId) {
+        logNicely("Booking car rental for " + lraId);
+
+        return Response.ok().build();
+//        return Response.status(500).build();
+    }
+
+    @Compensate
+    @PUT
+    @Path("/compensate")
+    public Response compensateCarRental(@HeaderParam(LRA.LRA_HTTP_CONTEXT_HEADER) String lraId) {
+        logNicely("Compensate for car rental " + lraId);
+
+        return Response.ok().build();
+    }
+
+    @Complete
+    @PUT
+    @Path("/complete")
+    public Response completeCarRental(@HeaderParam(LRA.LRA_HTTP_CONTEXT_HEADER) String lraId) {
+        logNicely("Complete for car rental " + lraId);
+
+        return Response.ok().build();
+    }
+
+    private void logNicely(String value) {
+        System.out.println("===============");
+        System.out.println("value = " + value);
+        System.out.println("===============");
+        System.out.println();
+    }
+}
---
+++ devoxxPL2022-lra/quarkus-lra-trip-example/hotel-service/src/main/java/io/xstefank/HotelResource.java
@@ -0,0 +1,66 @@
+package io.xstefank;
+
+import io.xstefank.client.CarRentalClient;
+import org.eclipse.microprofile.lra.annotation.Compensate;
+import org.eclipse.microprofile.lra.annotation.Complete;
+import org.eclipse.microprofile.lra.annotation.ws.rs.LRA;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.core.Response;
+
+@Path("/hotel")
+@ApplicationScoped
+public class HotelResource {
+
+    @Inject
+    @RestClient
+    CarRentalClient carRentalClient;
+
+    @LRA(value = LRA.Type.MANDATORY, end = false)
+    @POST
+    @Path("/book")
+    public Response bookHotel(@HeaderParam(LRA.LRA_HTTP_CONTEXT_HEADER) String lraId) {
+        logNicely("Booking hotel for " + lraId);
+
+        // propagate the saga to the car rental after the hotel is booked
+        try {
+            carRentalClient.bookCar(lraId);
+        } catch (Throwable throwable) {
+            // intentionally empty
+        }
+
+        return Response.ok().build();
+    }
+
+    @Compensate
+    @PUT
+    @Path("/compensate")
+    public Response compensateHotel(@HeaderParam(LRA.LRA_HTTP_CONTEXT_HEADER) String lraId) {
+        logNicely("Compensate for hotel " + lraId);
+
+        return Response.ok().build();
+    }
+
+    @Complete
+    @PUT
+    @Path("/complete")
+    public Response completeHotel(@HeaderParam(LRA.LRA_HTTP_CONTEXT_HEADER) String lraId) {
+        logNicely("Complete for hotel " + lraId);
+
+        return Response.ok().build();
+    }
+
+    private void logNicely(String value) {
+        System.out.println("===============");
+        System.out.println("value = " + value);
+        System.out.println("===============");
+        System.out.println();
+    }
+}
